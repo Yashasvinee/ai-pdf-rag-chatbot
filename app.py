975f52413a656e206b16d62a47f5b52088a55c74
Yashasvinee <modi.yashasvi2@gmail.com>
2026-07-03 15:46:36 +0530

    Implemented text chunking using LangChain

--- app.py
+++ app.py
@@ -1,5 +1,22 @@
-from utils.pdf_reader import extract_text_from_pdf
+# from utils.pdf_reader import extract_text_from_pdf
 
+# pdf_text = extract_text_from_pdf("data/sample.pdf")
+
+# print(pdf_text[:1000])
+from utils.pdf_reader import extract_text_from_pdf
+from utils.text_splitter import split_text_into_chunks
+
+# Read the PDF
 pdf_text = extract_text_from_pdf("data/sample.pdf")
 
-print(pdf_text[:1000])
+# Split into chunks
+chunks = split_text_into_chunks(pdf_text)
+
+# Print information
+print(f"Total Chunks: {len(chunks)}")
+
+print("\nFirst Chunk:\n")
+print(chunks[0])
+
+print("\nSecond Chunk:\n")
+print(chunks[1])
